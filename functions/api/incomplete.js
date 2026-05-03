8703e7e40f44024c0555bc0a53291a9d3d23bf9e
Giulex84 <giulianom.gm@gmail.com>
2026-05-03 15:39:11 +0200

    Update incomplete.js

--- functions/api/incomplete.js
+++ functions/api/incomplete.js
@@ -1,14 +1,23 @@
 export async function onRequest(context) {
   const apiKey = context.env.PI_API_KEY;
+  
+  // Legge l'header personalizzato inviato dal tuo frontend
   const userToken = context.request.headers.get("Authorization-User"); 
+
+  if (!userToken) {
+    return new Response(JSON.stringify({ error: "User Token missing in header" }), { status: 401 });
+  }
 
   const response = await fetch("https://api.minepi.com/v2/payments/incomplete", {
     method: "GET",
     headers: {
       "Authorization": `Key ${apiKey}`,
-      "accessToken": userToken 
+      "accessToken": userToken // Pi API si aspetta 'accessToken' qui
     }
   });
 
-  return new Response(JSON.stringify(await response.json()), { status: response.status });
+  const data = await response.json();
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
