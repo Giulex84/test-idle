7da3c846f2eb04fadf898adeb98236a543af8448
Giulex84 <giulianom.gm@gmail.com>
2026-05-04 13:31:18 +0200

    Improve error messages and token handling

--- functions/api/incomplete.js
+++ functions/api/incomplete.js
@@ -4,26 +4,30 @@ export async function onRequest(context) {
   const userToken = searchParams.get('token');
 
   if (!userToken) {
-    return new Response(JSON.stringify({ error: "Token non ricevuto" }), { status: 400 });
+    return new Response(JSON.stringify({ error: "Token non pervenuto al backend" }), { status: 400 });
   }
 
   try {
-    // Proviamo a inviare il token in ENTRAMBI i modi comuni
+    // Proviamo a inviare il token sia come 'accessToken' che come 'Authorization: Bearer'
+    // Alcuni endpoint di Pi preferiscono uno o l'altro
     const response = await fetch("https://api.minepi.com/v2/payments/incomplete", {
       method: "GET",
       headers: {
-        "Authorization": apiKey, // La tua Key con "Key "
-        "accessToken": userToken, // Metodo 1
-        "X-User-AccessToken": userToken // Metodo 2 (usato da alcuni SDK)
+        "Authorization": apiKey, // La tua chiave con "Key "
+        "accessToken": userToken, // Metodo standard Pi
+        "Bearer": userToken      // Metodo alternativo per alcuni nodi API
       }
     });
 
     const data = await response.json();
     return new Response(JSON.stringify(data), {
       status: response.status,
-      headers: { "Content-Type": "application/json" }
+      headers: { 
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*" 
+      }
     });
   } catch (err) {
-    return new Response(JSON.stringify({ error: "Errore di rete" }), { status: 500 });
+    return new Response(JSON.stringify({ error: "Errore di connessione server-to-server" }), { status: 500 });
   }
 }
