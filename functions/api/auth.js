341cdfeddac1d1c04e2b6af66f718b2a1c511789
Giulex84 <giulianom.gm@gmail.com>
2026-05-06 14:57:30 +0200

    Delete functions directory

--- functions/api/auth.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestPost(context) {
-  const { user } = await context.request.json();
-  // Qui puoi salvare l'utente nel tuo database se necessario
-  return new Response(JSON.stringify({ ok: true, username: user.username }));
-}
--- functions/api/clear.js
+++
@@ -1,5 +0,0 @@
-// Logica di emergenza per cancellare un pagamento che blocca tutto
-const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/cancel`, {
-  method: "POST",
-  headers: { "Authorization": `Key ${apiKey}` }
-});
